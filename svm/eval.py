5a942b16b3f744e78cf18482d2d35d1fcdf6124d
Zijing Zhang <zzj0402@gmail.com>
2023-03-19 00:23:54 +1300

    eval;lint folder

---
+++ svm/eval.py
@@ -0,0 +1,8 @@
+import pickle
+from sklearn import svm
+def load_model(filename):
+    with open(filename, 'rb') as f:
+        model = pickle.load(f)
+    return model
+clf=load_model('svm_model_9.pkl')
+print(clf.predict([[19,0,15,176,49,30.5,39.28,5535,1,0]]))
